3db0db362ba21f7ac237465f31001d7077297efa
YoanMen <yoan.menard.dev@gmail.com>
2024-01-23 15:27:23 +0100

    Working on JS for scroll and menu

--- script/navigation-marker.js
+++ script/navigation-marker.js
@@ -2,17 +2,67 @@ const marker = document.getElementById('marker-menu');
 const items = document.querySelectorAll('.menu-list__item');
 
 
-function fitMarker(event) {
-  const clickedItem = event.currentTarget;
+items.forEach(element => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
 
-  marker.style.left = clickedItem.offsetLeft + 'px';
-  marker.style.width = clickedItem.offsetWidth + 'px';
 
-}
+    const targetId = element.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
 
-items.forEach(item => {
-  item.addEventListener('click', fitMarker);
+    if (targetElement) {
+      const offset = window.innerHeight / 2 - 100;
+
+      window.scrollTo({
+        top: targetElement.offsetTop - offset,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+
+let currentSection;
+
+window.addEventListener('resize', function () {
+  getCurrentSection();
+
+});
+
+window.addEventListener('click', function () {
+  getCurrentSection();
+});
+
+window.addEventListener('scroll', function () {
+  getCurrentSection();
 });
 
 
 
+
+function getCurrentSection() {
+
+  const sections = document.querySelectorAll('section');
+  let current = null;
+  for (let index = 0; index < sections.length; index++) {
+
+    let rect = sections[index].getBoundingClientRect();
+
+
+    if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+      current = index;
+
+      marker.style.left = items[current].offsetLeft + 'px';
+      marker.style.width = items[current].offsetWidth + 'px';
+    }
+
+
+  }
+
+  if (current == null) {
+    marker.style.width = 0 + 'px';
+
+  }
+
+
+}
