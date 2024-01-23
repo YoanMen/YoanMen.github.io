9d28f50f0469a8e1e177c27e63a2beceda2b2d04
YoanMen <yoan.menard.dev@gmail.com>
2024-01-23 08:17:43 +0100

    refactoring css class

--- script/tabs.js
+++ script/tabs.js
@@ -1,6 +1,5 @@
-const projects = document.querySelectorAll('.project-container');
+const projects = document.querySelectorAll('.tab-container');
 
-let currentOpen;
 
 projects.forEach(element => {
   element.addEventListener('click', () => {
@@ -9,14 +8,10 @@ projects.forEach(element => {
 
     if (project.classList.contains('more-container--animation')) {
       project.classList.remove('more-container--animation');
+
     }
     else {
 
-      if (currentOpen != null) {
-        currentOpen.classList.remove('more-container--animation');
-      }
-
-      currentOpen = project;
       project.classList.add('more-container--animation');
     }
 
