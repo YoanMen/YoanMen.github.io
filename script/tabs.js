2fc77b338f566187921951c7b54ac8e350cc7177
YoanMen <yoan.menard.dev@gmail.com>
2024-01-23 12:31:31 +0100

    Begin implementation of Contact Section and some corrections

--- script/tabs.js
+++ script/tabs.js
@@ -5,14 +5,15 @@ projects.forEach(element => {
   element.addEventListener('click', () => {
 
     const project = element.querySelector('.more-container');
-
+    const arrowIcon = element.querySelector('.tab__icon');
     if (project.classList.contains('more-container--animation')) {
       project.classList.remove('more-container--animation');
-
+      arrowIcon.classList.remove('tab__icon--animation')
     }
     else {
 
       project.classList.add('more-container--animation');
+      arrowIcon.classList.add('tab__icon--animation')
     }
 
 
