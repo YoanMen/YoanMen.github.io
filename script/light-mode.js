b7a5fda5f86049614d26c8063abcf22f11af165c
YoanMen <yoan.menard.dev@gmail.com>
2024-01-24 16:59:16 +0100

    correction for apply correct light mode depending user preference

--- script/light-mode.js
+++ script/light-mode.js
@@ -2,10 +2,15 @@ const body = document.getElementById('body');
 const btn = document.querySelector('.theme__btn');
 
 
+
+if (window.matchMedia("(prefers-color-scheme: light)").matches)
+  body.classList.add('light-mode')
+
+
 btn.addEventListener('click', () => {
   body.classList.contains('light-mode') ? body.classList.remove('light-mode') : body.classList.add('light-mode');
-
-
 })
+
+
 
 
